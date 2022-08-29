abd4d7dcea3a140139854bbe4546cf9b2a3aadfc
АндрейСолуянов <soluyanov.a@yandex.ru>
2022-08-29 17:17:14 +0300

    Сортировка

--- src/pages/Gallerey.js
+++ src/pages/Gallerey.js
@@ -6,6 +6,16 @@ const Gallerey = (props) =>  {
 
 const [post, setPosts] = useState([]);
 
+let Sort = () => { 
+
+  let newArray = post.slice();
+  newArray.sort((a, b)=>{return a.rating - b.rating});
+  newArray.reverse();
+  setPosts(newArray);
+
+
+};
+
 useEffect(() => {
 
     fetch('http://localhost/games', {
@@ -24,25 +34,28 @@ useEffect(() => {
   return(
     
   <div id="family">
-    <div id="sort"><img  src="images/sort.png"  width="30" height="30" title ="Сортировать"></img></div>
-  {
-    post.map((index)=>{
-      
-    return(  
-    <div className="responsive" key={index.id}>
-      <div className="gallery">
-      <Link to= {`/game/${index.slug}`}>
-        <img className="img" src={index.poster} alt="Northern Lights" width="600" height="400"></img>
-      </Link>
-        <div>
-          <div className="Completed_name">{index.name}</div>
-          <div><img className="like" src="images/like.png"  width="10" height="10"></img>{index.rating}</div>
+    <div id="sort">
+      <img onClick={() => Sort()} src="images/sort.png"  width="30" height="30" title ="Сортировать"></img>
+    </div>
+    {
+      post.map((index)=>{
+        
+      return(  
+      <div className="responsive" key={index.id}>
+        <div className="gallery">
+
+        <Link to= {`/game/${index.slug}`}>
+          <img className="img" src={index.poster}  alt="Northern Lights" width="600" height="400"></img>
+        </Link>
+          <div>
+            <div className="Completed_name">{index.name}</div>
+            <div><img className="like" src="images/like.png"  width="10" height="10"></img>{index.rating}</div>
+          </div>
         </div>
       </div>
-    </div>)})
-  }
+      )})
+    }
   </div>
-
   )
 
   
