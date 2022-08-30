5629bb566f0e12c6efe37dd1d9c7de95226e68e7
АндрейСолуянов <soluyanov.a@yandex.ru>
2022-08-30 14:18:33 +0300

    Содание Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,23 @@ import React from "react"
 import './Home.css';
 
 const Home = () => {
+    
 
-    return <div>You are in Home page</div>
+    return (
+        <form>
+        <div className = "flex">
+            <div className = "flex-textarea-button">
+            <div className="blok1">
+                <form id="form" action="">
+                    <textarea spellcheck="false" className="colortext" name="comment">Любое значение для получения токена.</textarea>
+                </form>
+            </div>
+            <div className="blok2">
+              <button className="button">Ввод</button>
+            </div>
+            </div>
+        </div>
+        </form>
+    )
 }
 export default Home;
